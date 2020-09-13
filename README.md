12420640c1b9a0d285f771f710d72d97cd47d904
Amit P Naik <amit.naik8103@gmail.com>
2020-09-13 22:58:31 +0530

    feat: web component widget

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 [![CircleCI Status](https://circleci.com/gh/es-hackathon/bookmark-dashboard-widget.svg?style=shield&circle-token=71828a7c9872f3347186c8ff7097160b6889853f)](https://app.circleci.com/pipelines/github/es-hackathon/bookmark-dashboard-widget)
-[![npm version](https://img.shields.io/npm/v/react.svg?style=flat)](https://www.npmjs.com/package/react)
+[![react version](https://img.shields.io/npm/v/react.svg?style=flat)](https://www.npmjs.com/package/react)
 [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/facebook/react/blob/master/LICENSE)
 
 
--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactWebComponent from 'react-web-component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className='card'>
+        <div className='card-inner'>
+          <div className='card-front'>
+            <div className="card border-dark mb-3">
+              <div className="card-body text-dark">
+                <h5 className="card-title">Github{this.props.title}</h5>
+                <div className="row card-body-height"   ><div className="col-sm-2">  <i className="fa fa-bookmark fa-3x" aria-hidden="true"></i></div>
+                  <div className="col-sm-10"  > <p className="card-text">{this.props.description}</p> </div>
+                </div>
+                <div className="row"  >
+                  <div className="col-sm-6"><small className=" card-text-small text-muted">Added 10 Sep 2020</small></div>
+                  <div className="col-sm-6 text-right">    <i className="fa fa-share-alt" aria-hidden="true"></i> </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='card-back'>
+            <div>
+              <div className="card-body">
+                <h5 className="card-title">{this.props.title}</h5>
+                <p className="card-text card-body-back-height">{this.props.description}</p>
+              </div>
+              <div className="row"  >
+                <div className="col-sm-3"><i className="fa fa-star" aria-hidden="true"></i></div>
+                <div className="col-sm-6 text-center">
+                  Redirect
+                </div>
+                <div className="col-sm-3 text-right"><i className="fa fa-share-alt-square" aria-hidden="true"></i> </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+// export default App;
+// ReactWebComponent.create(<App name="Mars" />, 'another-component');
